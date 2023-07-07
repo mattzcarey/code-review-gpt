64581442b0e9d755eba19b94516cc5eec08a3b70
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-07-07 15:38:01 +0100

    feat: gh action

---
+++ src/args.ts
@@ -0,0 +1,16 @@
+import yargs from "yargs";
+
+const argv = yargs
+  .option("ci", {
+    description: "Indicate that the script is running on a CI environment",
+    type: "boolean",
+  })
+  .parseSync();
+
+export const gitCommand = (): string => {
+  if (argv.ci) {
+    return "git diff --name-only origin/main";
+  } else {
+    return "git diff --name-only --cached";
+  }
+};
