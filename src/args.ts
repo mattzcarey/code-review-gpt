5b81c2c1d7ca5135975e94a649c71d85b99bcd88
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-07-07 16:48:05 +0100

    tessttt

--- src/args.ts
+++ src/args.ts
@@ -1,16 +1,26 @@
 import yargs from "yargs";
 
+import yargs from "yargs";
+
 const argv = yargs
   .option("ci", {
     description: "Indicate that the script is running on a CI environment",
     type: "boolean",
   })
+  .option("base_sha", {
+    description: "The base SHA",
+    type: "string",
+  })
+  .option("github_sha", {
+    description: "The GitHub SHA",
+    type: "string",
+  })
   .parseSync();
 
 export const gitCommand = (): string => {
   if (argv.ci) {
-    return "git diff --name-only main ${{ github.event.pull_request.head.ref }}";
+    return `git diff --name-only --diff-filter=ACMRT ${argv.base_sha} ${argv.github_sha}`;
   } else {
-    return "git diff --name-only --cached";
+    return "git diff --name-only --diff-filter=ACMRT --cached";
   }
 };
