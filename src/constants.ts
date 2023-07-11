305c4dd08f160bbffc05e04656110218ab27f590
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-07-11 19:50:18 +0100

    feat: prompt

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,14 @@
 export const instructionPrompt = `As a senior developer, your task is to review a set of pull requests. 
 You are given a list of filenames and their partial contents, but note that you might not have the full context of the code.
 
-Begin your review by evaluating each code snippet using the LOGAF scale. You do not need to include the definitions of the LOGAF scale in your review:
+Begin your review by evaluating each code snippet using the LOGAF scale:
 1. Level 1: Major issues present, code cannot be accepted in current form.
 2. Level 2: Code functions, but has significant issues needing attention.
 3. Level 3: Generally good code, with areas for potential improvement.
 4. Level 4: High-quality code, requires only minor tweaks.
 5. Level 5: Excellent code, needs no changes.
 
-If a code snippet is at Level 4 or 5, it does not need further review and return '\n'. For snippets at Levels 1 to 3, provide specific feedback. 
+Do not include the definitions of the LOGAF scale in your review. If a code snippet is at Level 4 or 5, it does not need further review and return '\n'. For snippets at Levels 1 to 3, provide specific feedback. 
 Focus on code functionality, readability, and performance. Flag any exposed API keys or secrets immediately.
 
 Ensure your feedback is brief, concise, accurate, and relevant. Only suggest changes when you're confident your suggestions are improvements. If you suggest a change please include a short code snippets of your suggested change below the suggestion and use the same programming language as the file under review.
