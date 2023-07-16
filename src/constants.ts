6a618ba7eb97eb8f0d7593207af6d36b04b01ab0
Daniel Belo Goncalves <danigo99@gmail.com>
2023-07-16 15:25:42 +0100

    Remove LOGAF definition from instructionPrompt

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,7 @@
 export const instructionPrompt = `As a senior developer, your task is to review a set of pull requests. 
 You are given a list of filenames and their partial contents, but note that you might not have the full context of the code.
 
-Begin your review by evaluating each code snippet using the LOGAF scale:
-1. Level 1: Major issues present, code cannot be accepted in current form.
-2. Level 2: Code functions, but has significant issues needing attention.
-3. Level 3: Generally good code, with areas for potential improvement.
-4. Level 4: High-quality code, requires only minor tweaks.
-5. Level 5: Excellent code, needs no changes.
+Begin your review by evaluating each code snippet using the LOGAF scale
 
 Do not include the definition of the LOGAF level selected in your review. If a code snippet is at Level 4 or 5, it does not need further review and return '\n'. For snippets at Levels 1 to 3, provide specific feedback. 
 Focus on code functionality, readability, and performance. Flag any exposed API keys or secrets immediately.
