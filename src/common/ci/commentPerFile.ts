51bc1a1f1dc2f5fa83ab5dc3a6e48f3528dc98b3
Liza Cullis <lizac@aleios.com>
2023-07-27 10:21:40 +0200

    restructure helper methods into a utils file

--- src/common/ci/commentPerFile.ts
+++ src/common/ci/commentPerFile.ts
@@ -1,6 +1,5 @@
-import { context, getOctokit } from "@actions/github";
 import { IFeedback } from "../../review/llm/feedbackProcessor";
-import { getRelativePath, getToken } from "./utils";
+import { commentOnFile, getOctokitRepoDetails } from "./utils";
 
 /**
  * Publish comments on a file-by-file basis on the pull request. If the bot has already commented on a file (i.e. a comment with the same sign off exists on that file), update the comment instead of creating a new one.
@@ -9,66 +8,39 @@ import { getRelativePath, getToken } from "./utils";
  * @param signOff The sign off to use. This also serves as key to check if the bot has already commented and update the comment instead of posting a new one if necessary.
  * @returns void
  */
-export const commentPerFile = async (feedbacks: IFeedback[], signOff: string) => {
+export const commentPerFile = async (
+  feedbacks: IFeedback[],
+  signOff: string
+) => {
   try {
-    const githubToken = getToken();
-    const { payload, issue } = context;
+    const octokitRepoDetails = getOctokitRepoDetails();
+    if (octokitRepoDetails) {
+      const { octokit, owner, repo, pull_number } = octokitRepoDetails;
 
-    if (!payload.pull_request) {
-      console.warn("Not a pull request. Skipping commenting on PR...");
-      return;
-    }
+      // Get PR and commit_id
+      const pullRequest = await octokit.rest.pulls.get({
+        owner,
+        repo,
+        pull_number: pull_number,
+      });
+      const commit_id = pullRequest.data.head.sha;
 
-    const octokit = getOctokit(githubToken);
-    const { owner, repo, number: pull_number } = issue;
-
-    // Get PR and commit_id
-    const pullRequest = await octokit.rest.pulls.get({
-      owner,
-      repo,
-      pull_number: pull_number,
-    });
-    const commit_id = pullRequest.data.head.sha;
-
-    // Comment all feedback line by line
-    for (const feedback of feedbacks) {
-      try {
-        const botCommentBody = `${feedback.details}\n\n---\n\n${signOff}`;
-
-        const { data: comments } = await octokit.rest.pulls.listReviewComments({
-          owner,
-          repo,
-          pull_number,
-        });
-        
-        // Check if bot has already commented on this file
-        const relativePath = getRelativePath(feedback.fileName, issue.repo);
-        const botComment = comments.find((comment) =>
-        comment?.path === relativePath && comment?.body?.includes(signOff)
-        );
-
-        if (botComment) {
-          octokit.rest.pulls.updateReviewComment({
-            owner,
-            repo,
-            comment_id: botComment.id,
-            body: botCommentBody,
-          });
-        } else {
-          await octokit.rest.pulls.createReviewComment({
+      // Comment all feedback file by file
+      for (const feedback of feedbacks) {
+        try {
+          commentOnFile(octokit, {
+            feedback,
+            signOff,
             owner,
             repo,
             pull_number,
-            body: botCommentBody,
             commit_id,
-            path: relativePath,
-            subject_type: "file",
           });
+        } catch (error) {
+          console.error(
+            `Failed to comment on PR for feedback: ${feedback.details}. Error: ${error}`
+          );
         }
-      } catch (error) {
-        console.error(
-          `Failed to comment on PR for feedback: ${feedback.details}. Error: ${error}`
-        );
       }
     }
   } catch (error) {
--- src/common/ci/utils.ts
+++ src/common/ci/utils.ts
@@ -1,11 +1,14 @@
 import { getGitHubEnvVariables } from "../../config";
+import { context } from "@actions/github";
+import { Octokit } from "octokit";
+import { IFeedback } from "../../review/llm/feedbackProcessor";
 
 export const getRelativePath = (fileName: string, repoName: string): string => {
   const repoIndex = fileName.lastIndexOf(repoName);
   if (repoIndex !== -1) {
     return fileName.slice(repoIndex + repoName.length + 1);
   } else {
-    // If the repository name is not found in the absolute path, return the original absolute path.
+    // If the repo name is not in the absolute path, return the original absolute path.
     return fileName;
   }
 };
@@ -16,4 +19,65 @@ export const getToken = () => {
     throw new Error("GITHUB_TOKEN is not set");
   }
   return githubToken;
+};
+
+export const getOctokitRepoDetails = () => {
+  const githubToken = getToken();
+  const { payload, issue } = context;
+
+  if (!payload.pull_request) {
+    console.warn("Not a pull request. Skipping commenting on PR...");
+    return;
+  }
+  const octokit = new Octokit({ auth: githubToken });
+  const { owner, repo, number: pull_number } = issue;
+  return { octokit, owner, repo, pull_number };
+};
+
+export const commentOnFile = async (
+  octokit: Octokit,
+  data: CreateFileCommentData
+) => {
+  const botCommentBody = `${data.feedback.details}\n\n---\n\n${data.signOff}`;
+
+  const { data: comments } = await octokit.rest.pulls.listReviewComments({
+    owner: data.owner,
+    repo: data.repo,
+    pull_number: data.pull_number,
+  });
+
+  // Check if bot has already commented on this file
+  const relativePath = getRelativePath(data.feedback.fileName, data.repo);
+  const botComment = comments.find(
+    (comment) =>
+      comment?.path === relativePath && comment?.body?.includes(data.signOff)
+  );
+
+  if (botComment) {
+    octokit.rest.pulls.updateReviewComment({
+      owner: data.owner,
+      repo: data.repo,
+      comment_id: botComment.id,
+      body: botCommentBody,
+    });
+  } else {
+    await octokit.rest.pulls.createReviewComment({
+      owner: data.owner,
+      repo: data.repo,
+      pull_number: data.pull_number,
+      body: botCommentBody,
+      commit_id: data.commit_id,
+      path: relativePath,
+      subject_type: "file",
+    });
+  }
+};
+
+export type CreateFileCommentData = {
+  feedback: IFeedback;
+  signOff: string;
+  owner: string;
+  repo: string;
+  pull_number: number;
+  commit_id: string;
 };
