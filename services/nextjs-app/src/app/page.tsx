1cc6fe70c4344e613222199a910b0b3f4202d9dd
Sebastian Oliver <sebo@aleios.com>
2023-08-02 16:21:00 +0200

    chore: remove unnecessary styling

--- services/nextjs-app/src/app/page.tsx
+++ services/nextjs-app/src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px]">
-        <h1>Code Review GPT</h1>
+      <div className="relative flex place-items-center">
+        <h1 className="text-4xl">Code Review GPT</h1>
       </div>
     </main>
   )
