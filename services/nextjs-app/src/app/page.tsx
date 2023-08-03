e8a1085b2b776619aa0d11d1b2e10eec49f5f172
Sebastian Oliver <sebo@aleios.com>
2023-08-03 11:25:43 +0200

    feat: header styling and linking to the repo

--- services/nextjs-app/src/app/page.tsx
+++ services/nextjs-app/src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative flex place-items-center">
-        <h1 className="text-4xl">Code Review GPT</h1>
+      <div className="relative flex place-items-center hover:scale-125 duration-500">
+        <h1 className="hover:text-purple-600 text-4xl font-mono">
+          <a href="https://github.com/mattzcarey/code-review-gpt">Code Review GPT</a>
+        </h1>
       </div>
     </main>
   )
