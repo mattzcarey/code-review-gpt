16520890a3d3d54d94dd611ec36d7d52642770ad
Sebastian Oliver <sebo@aleios.com>
2023-08-10 18:15:24 +0200

    chore: removing unneeded content

--- services/web-app/src/components/footer/footer.tsx
+++ services/web-app/src/components/footer/footer.tsx
@@ -2,10 +2,9 @@ import Image from "next/image";
 
 export const Footer = (): JSX.Element => {
   return (
-    <footer className="flex flex-row justify-evenly items-center text-xl">
-      <div>
-        Where you can find us
-        <div className="flex flex-row justify-evenly py-[10px]">
+    <footer className="flex flex-col items-center text-xl">
+      Join the community
+      <div className="flex flex-row justify-evenly py-[10px]">
           <a href="https://github.com/mattzcarey/code-review-gpt">
             <Image src="/githubLogo.svg" alt={"github logo"} width={40} height={40} />
           </a>
@@ -15,18 +14,6 @@ export const Footer = (): JSX.Element => {
           <a href="https://join.slack.com/t/orion-tools/shared_invite/zt-20x79nfgm-UGIHK1uWGQ59JQTpODYDwg">
             <Image src="/slackLogo.svg" alt={"slack logo"} width={40} height={40} className="scale-[2]" />
           </a>
-        </div>
-      </div>
-      <div>
-        Our Sponsors ❤️
-        <div className="flex flex-row justify-evenly py-[10px]">
-          <a href="https://www.quivr.app/">
-            <Image src="https://github.com/mattzcarey/code-review-gpt/assets/77928207/30361248-3159-4535-8efb-b114989ae886" alt={"quiver logo"} width={40} height={40} />
-          </a>
-          <a href="https://www.aleios.com/">
-            <Image src="https://github.com/mattzcarey/code-review-gpt/assets/77928207/a47c2460-b866-433f-a4c9-efb5737d4fed" alt={"aleios logo"} width={40} height={40} />
-          </a>
-        </div>
       </div>
     </footer>
   );
