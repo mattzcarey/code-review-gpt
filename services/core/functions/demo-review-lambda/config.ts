304b1a6b87b8583522110c6be9efc8cd5e547d0d
Manon Faour <manonf@aleios.com>
2023-08-10 10:51:26 +0100

    feat: add demo review lambda

---
+++ services/core/functions/demo-review-lambda/config.ts
@@ -0,0 +1,38 @@
+import { Duration, Stack } from "aws-cdk-lib";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Architecture, Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import { join } from "path";
+import { buildResourceName } from "../../helpers";
+import { OPENAI_API_KEY_PARAM_NAME } from "../../constants";
+
+export class DemoReviewLambda extends NodejsFunction {
+  constructor(scope: Construct, id: string) {
+    super(scope, id, {
+      functionName: buildResourceName(id),
+      entry: join(__dirname, "index.ts"),
+      handler: "main",
+      runtime: Runtime.NODEJS_18_X,
+      architecture: Architecture.ARM_64,
+      environment: {
+        OPENAI_API_KEY_PARAM_NAME: OPENAI_API_KEY_PARAM_NAME,
+      },
+      timeout: Duration.seconds(60),
+    });
+
+    // Grant the Lambda function permissions to access Parameter Store
+    const parameterStoreArn = Stack.of(scope).formatArn({
+      service: "ssm",
+      resource: "parameter",
+      resourceName: OPENAI_API_KEY_PARAM_NAME,
+    });
+
+    this.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["ssm:GetParameter"],
+        resources: [parameterStoreArn],
+      })
+    );
+  }
+}
