24fcde4837e94a7749be026be484a427916d1cd4
Liza Cullis <lizac@aleios.com>
2023-08-10 12:35:12 +0200

    feat: added apigateway events support

--- services/core/functions/review-lambda/index.ts
+++ services/core/functions/review-lambda/index.ts
@@ -1,25 +1,41 @@
 import { review } from "../../../../src/review/index";
 import { ReviewArgs, ReviewFile } from "../../../../src/common/types";
 import { getOpenAiApiEnvVariable } from "./helpers";
+import { APIGatewayProxyEvent } from "aws-lambda";
 
 interface ReviewLambdasBody {
   args: ReviewArgs;
   files: ReviewFile[];
 }
 
-export const main = async (event: ReviewLambdasBody) => {
+export const main = async (event: APIGatewayProxyEvent) => {
   if (process.env.OPENAI_API_KEY_PARAM_NAME === undefined) {
     throw new Error(
       "OPENAI_API_KEY_PARAM_NAME environment variable is not set."
     );
   }
-  if (event === null) {
-    throw new Error("Request body is null");
+  if (event.body === null) {
+    return Promise.resolve({
+      statusCode: 400,
+      body: "The request does not contain a body as expected.",
+    });
   }
-  const keyValue = await getOpenAiApiEnvVariable(
-    process.env.OPENAI_API_KEY_PARAM_NAME
-  );
 
-  process.env["OPENAI_API_KEY"] = keyValue;
-  return await review(event.args, event.files);
+  try {
+    const inputBody = JSON.parse(event.body) as ReviewLambdasBody;
+    console.log(inputBody);
+    const keyValue = await getOpenAiApiEnvVariable(
+      process.env.OPENAI_API_KEY_PARAM_NAME
+    );
+
+    process.env["OPENAI_API_KEY"] = keyValue;
+    return await review(inputBody.args, inputBody.files);
+  } catch (err) {
+    console.error(err);
+
+    return Promise.resolve({
+      statusCode: 500,
+      body: "Error when reviewing code.",
+    });
+  }
 };
