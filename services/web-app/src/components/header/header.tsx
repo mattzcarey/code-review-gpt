1c885d8d3de12d3f226a332552f32b78470dc7b5
Sebastian Oliver <sebo@aleios.com>
2023-08-11 10:17:28 +0200

    feat: signOut button and showing users email when signed in

--- services/web-app/src/components/header/header.tsx
+++ services/web-app/src/components/header/header.tsx
@@ -1,15 +1,26 @@
 "use client"
 import Image from "next/image";
-import { LoginButton } from '@/components/buttons/login';
+import { SignInButton } from '@/components/buttons/signIn';
+import { useSession } from 'next-auth/react';
+import { SignOutButton } from '../buttons/signOut';
 
 export const Header = (): JSX.Element => {
+  const { data: session } = useSession();
+
   return (
     <header className="flex flex-row navbar justify-between items-center">
       <Image src="/icon.png" alt={"orion logo"} width={100} height={100} />
       <h1 className="text-4xl font-mono">
           Code Review GPT
       </h1>
-      <LoginButton />
+      { session ? (
+        <div className="flex flex-col">
+          {session.user?.email}
+          <SignOutButton />
+        </div>
+      ) : (
+        <SignInButton />
+      )}
     </header>
   );
 };
