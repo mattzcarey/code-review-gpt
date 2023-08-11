b108a06ae69316f49b3b6d143e3f465a3837923c
Liza Cullis <lizac@aleios.com>
2023-08-11 15:01:10 +0200

    feat: authentication for review-lambda

--- services/core/functions/review-lambda/config.ts
+++ services/core/functions/review-lambda/config.ts
@@ -8,6 +8,7 @@ import { buildResourceName } from "../../helpers";
 import {
   LANGCHAIN_API_KEY_PARAM_NAME,
   OPENAI_API_KEY_PARAM_NAME,
+  GITHUB_SECRET_PARAM_NAME,
 } from "../../constants";
 
 export class ReviewLambda extends NodejsFunction {
@@ -20,6 +21,7 @@ export class ReviewLambda extends NodejsFunction {
       architecture: Architecture.ARM_64,
       environment: {
         OPENAI_API_KEY_PARAM_NAME: OPENAI_API_KEY_PARAM_NAME,
+        GITHUB_SECRET_PARAM_NAME: GITHUB_SECRET_PARAM_NAME,
         LANGCHAIN_API_KEY_PARAM_NAME: LANGCHAIN_API_KEY_PARAM_NAME,
         LANGCHAIN_TRACING_V2: "true",
         LANGCHAIN_PROJECT: "code-review-gpt",
@@ -28,13 +30,19 @@ export class ReviewLambda extends NodejsFunction {
     });
 
     // Grant the Lambda function permissions to access Parameter Store
-    const openAIApiKeyparameterStoreArn = Stack.of(scope).formatArn({
+    const openAIApiKeyParameterStoreArn = Stack.of(scope).formatArn({
       service: "ssm",
       resource: "parameter",
       resourceName: OPENAI_API_KEY_PARAM_NAME,
     });
 
-    const langchainApiKeyparameterStoreArn = Stack.of(scope).formatArn({
+    const githubSecretParameterStoreArn = Stack.of(scope).formatArn({
+      service: "ssm",
+      resource: "parameter",
+      resourceName: GITHUB_SECRET_PARAM_NAME,
+    });
+
+    const langchainApiKeyParameterStoreArn = Stack.of(scope).formatArn({
       service: "ssm",
       resource: "parameter",
       resourceName: LANGCHAIN_API_KEY_PARAM_NAME,
@@ -44,8 +52,9 @@ export class ReviewLambda extends NodejsFunction {
       new PolicyStatement({
         actions: ["ssm:GetParameter"],
         resources: [
-          openAIApiKeyparameterStoreArn,
-          langchainApiKeyparameterStoreArn,
+          openAIApiKeyParameterStoreArn,
+          githubSecretParameterStoreArn,
+          langchainApiKeyParameterStoreArn,
         ],
       })
     );
--- services/core/functions/review-lambda/index.ts
+++ services/core/functions/review-lambda/index.ts
@@ -3,6 +3,7 @@ import { ReviewArgs, ReviewFile } from "../../../../src/common/types";
 import { APIGatewayProxyEvent } from "aws-lambda";
 import { getVariableFromSSM } from "../helpers";
 import { logger } from "../../../../src/common/utils/logger";
+import { authenticate } from "./auth";
 
 interface ReviewLambdasBody {
   args: ReviewArgs;
@@ -12,6 +13,28 @@ interface ReviewLambdasBody {
 logger.settings.minLevel = 4;
 
 export const main = async (event: APIGatewayProxyEvent) => {
+  if (event.body === null) {
+    return Promise.resolve({
+      statusCode: 400,
+      body: "The request does not contain a body as expected.",
+    });
+  }
+
+  if (event.headers['X-Hub-Signature-256'] === undefined) {
+    return Promise.resolve({
+      statusCode: 401,
+      body: "No authentication token found.",
+    });
+  }
+
+  const authenticated = authenticate(event.headers['X-Hub-Signature-256'], event.body);
+  if (!authenticated){
+    return Promise.resolve({
+      statusCode: 401,
+      body: "Unauthorized.",
+    });
+  }
+ 
   const openAIApiKey = await getVariableFromSSM(
     process.env.OPENAI_API_KEY_PARAM_NAME ?? ""
   );
@@ -20,12 +43,6 @@ export const main = async (event: APIGatewayProxyEvent) => {
     process.env.LANGCHAIN_API_KEY_PARAM_NAME ?? ""
   );
 
-  if (event.body === null) {
-    return Promise.resolve({
-      statusCode: 400,
-      body: "The request does not contain a body as expected.",
-    });
-  }
 
   try {
     const inputBody = JSON.parse(event.body) as ReviewLambdasBody;
