fceddc38ab276250839311460c362dd939f970ea
Manon Faour <manonf@aleios.com>
2023-08-14 11:51:59 +0100

    feat: change entities from functions to const

---
+++ services/core/entities/ReviewEntity.ts
@@ -0,0 +1,16 @@
+import { Entity } from "dynamodb-toolbox";
+import { Table } from "./Table";
+
+export const ReviewEntity = new Entity({
+  name: "demoReviewS3LocationEntity",
+  attributes: {
+    PK: { partitionKey: true, hidden: true, prefix: "DEMOREVIEWID#" },
+    SK: { sortKey: true, hidden: true, default: "S3_LOCATION" },
+    reviewId: ["PK", 0, { type: "string", required: true }],
+    inputLocation: { type: "string", required: true },
+    responseLocation: { type: "string", required: true },
+    demo: { type: "boolean", default: false },
+    date: { type: "string", default: () => new Date().toDateString() },
+  },
+  table: Table,
+} as const);
--- services/core/entities/index.ts
+++ services/core/entities/index.ts
@@ -1,2 +1,3 @@
-export { getUserEntity } from "./userEntity";
-export { getDemoReviewDayCounterEntity } from "./demoReviewDayCounterEntity";
+export { UserEntity } from "./UserEntity";
+export { ReviewDemoCounterEntity } from "./ReviewDemoCounterEntity";
+export { ReviewEntity } from "./ReviewEntity";
