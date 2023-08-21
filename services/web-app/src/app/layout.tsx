fd75b647f3e58d436db609528bf16cbc10211a74
lizacullis <126663266+lizacullis@users.noreply.github.com>
2023-08-21 11:03:56 +0200

    Revert "feat: User Info Page with dummy data"

--- services/web-app/src/app/layout.tsx
+++ services/web-app/src/app/layout.tsx
@@ -1,36 +1,28 @@
 import "../styles/globals.css";
-import "../styles/custom.css";
-import React, { ReactNode } from "react";
-import { NextAuthProvider } from "./providers";
-import { Footer } from "@/components/footer/footer";
-import { Header } from "@/components/header/header";
-import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+import React, { ReactNode } from 'react';
+import { NextAuthProvider } from './providers';
+import { Footer } from '@/components/footer/footer';
+import { Header } from '@/components/header/header';
 
 export const metadata = {
-  title: "Code Review GPT",
-};
+  title: 'Code Review GPT',
+}
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+  }: {children: ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <script
-          type="module"
-          src="https://md-block.verou.me/md-block.js"
-        ></script>
-      </head>
-      <body className="flex flex-col min-h-screen">
-        <Theme>
+        <head>
+        <script type="module" src="https://md-block.verou.me/md-block.js"></script>
+        </head>
+        <body>
           <NextAuthProvider>
             <Header />
-            <main className="flex flex-col flex-grow mb-16"> 
-              {children}
-            </main>
+            {children}
             <Footer />
           </NextAuthProvider>
-        </Theme>
-      </body>
+        </body>
     </html>
-  );
+  )
 }
--- services/web-app/src/app/page.tsx
+++ services/web-app/src/app/page.tsx
@@ -1,10 +1,11 @@
+import { useSession } from 'next-auth/react';
 import { InstallationInstructions } from './constants';
 import ReactMarkdown from 'react-markdown';
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col p-5 ml-10 mr-10">
+      <div className="flex flex-col p-5 ">
         <ReactMarkdown className="markdown" children={InstallationInstructions} />
       </div>
     </>
