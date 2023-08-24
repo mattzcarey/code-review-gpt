db6bf9c2e30e17c3bb56127a8abfe4288b3bd30b
Liza Cullis <lizac@aleios.com>
2023-08-24 10:18:59 +0200

    chore: update to use div instead of <>

--- services/web-app/src/components/cards/returnToHome.tsx
+++ services/web-app/src/components/cards/returnToHome.tsx
@@ -5,7 +5,7 @@ interface ReturnToHomeProps {
 }
 export const ReturnToHome: React.FC<ReturnToHomeProps> = ({ message }) => {
   return (
-    <>
+    <div>
       <p className="text-xl flex justify-center mt-16 ml-10">{message}</p>
       <Link
         className="text-xl underline flex justify-center mb-5 ml-10"
@@ -13,6 +13,6 @@ export const ReturnToHome: React.FC<ReturnToHomeProps> = ({ message }) => {
       >
         Click here to return to home page.
       </Link>
-    </>
+    </div>
   );
 };
