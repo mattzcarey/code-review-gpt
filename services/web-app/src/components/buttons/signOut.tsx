ac03024faa22207eed0abf03f1c81a82a44be3f9
Liza Cullis <lizac@aleios.com>
2023-08-24 15:03:50 +0200

    feat: Added redirect for signin and signout

--- services/web-app/src/components/buttons/signOut.tsx
+++ services/web-app/src/components/buttons/signOut.tsx
@@ -1,9 +1,15 @@
-import { signOut } from 'next-auth/react';
+import { signOut } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export const SignOutButton = () => {
-return (
-  <button className="border-2 border-black p-[5px] mx-3 text-xl hover:underline" onClick={() => { signOut() }}>
-    Sign Out
-  </button>
-  )
+  return (
+    <button
+      className="border-2 border-black p-[5px] mx-3 text-xl hover:underline"
+      onClick={() => {
+        signOut({ callbackUrl: "/" });
+      }}
+    >
+      Sign Out
+    </button>
+  );
 };
