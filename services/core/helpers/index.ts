5f20df4fd2198745a6c468e0aaaad1a989116b8b
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-08-24 17:09:00 +0100

    wip: fix

--- services/core/helpers/index.ts
+++ services/core/helpers/index.ts
@@ -1,4 +1,5 @@
 export * from "./certificates";
 export * from "./domains";
 export * from "./env-helpers";
+export * from "./format-response";
 export * from "./isProduction";
--- services/web-app/src/lib/auth.ts
+++ services/web-app/src/lib/auth.ts
@@ -1,10 +1,10 @@
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb"
-import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter"
+import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBAdapter } from "@next-auth/dynamodb-adapter";
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import { Table } from "sst/node/table";
 import { Config } from "sst/node/config";
+import { Table } from "sst/node/table";
 
 const dynamoClient = DynamoDBDocument.from(new DynamoDB({}), {
   marshallOptions: {
@@ -12,16 +12,18 @@ const dynamoClient = DynamoDBDocument.from(new DynamoDB({}), {
     removeUndefinedValues: true,
     convertClassInstanceToMap: true,
   },
-})
+});
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
-      clientId: Config.GITHUB_ID || process.env.GITHUB_ID as string,
-      clientSecret: Config.GITHUB_SECRET || process.env.GITHUB_SECRET as string,
+      clientId: Config.GITHUB_ID || (process.env.GITHUB_ID as string),
+      clientSecret:
+        Config.GITHUB_SECRET || (process.env.GITHUB_SECRET as string),
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   adapter: DynamoDBAdapter(dynamoClient, {
-    tableName: Table['user-data'].tableName,
+    tableName: Table["user-data"].tableName,
   }),
 };
