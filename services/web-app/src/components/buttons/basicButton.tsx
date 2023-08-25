d50e1552e80145f54e3d5fa6918076f48945022f
Liza Cullis <lizac@aleios.com>
2023-08-25 18:25:50 +0200

    feat: updated styling for header and buttons

--- services/web-app/src/components/buttons/basicButton.tsx
+++ services/web-app/src/components/buttons/basicButton.tsx
@@ -7,22 +7,19 @@ export interface UpdateAPIKeyProps {
   onClick?: () => void;
 }
 
-const BasicButton: React.FC<UpdateAPIKeyProps> = React.forwardRef(
-  ({ text, onClick }, ref) => {
-    return (
-      <Button
-        ref={ref}
-        className="font-light"
-        highContrast
-        color="gray"
-        variant="solid"
-        size="3"
-        onClick={onClick}
-      >
-        {text}
-      </Button>
-    );
-  }
-);
+const BasicButton: React.FC<UpdateAPIKeyProps> = ({ text, onClick }) => {
+  return (
+    <Button
+      className="font-light"
+      highContrast
+      color="gray"
+      variant="solid"
+      size="3"
+      onClick={onClick}
+    >
+      {text}
+    </Button>
+  );
+};
 
 export default BasicButton;
