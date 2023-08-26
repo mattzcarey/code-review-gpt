13ef803dc7143f516f3fb5865048fcb5a3de8d5a
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-08-26 18:54:14 +0100

    fix: arecords

--- services/core/resources/constructs/api-gateway.ts
+++ services/core/resources/constructs/api-gateway.ts
@@ -1,11 +1,22 @@
-import { RestApi, RestApiProps } from "aws-cdk-lib/aws-apigateway";
+import {
+  EndpointType,
+  RestApi,
+  RestApiProps,
+} from "aws-cdk-lib/aws-apigateway";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
+import { ApiGateway } from "aws-cdk-lib/aws-route53-targets";
 import { Construct } from "constructs";
 import { buildResourceName } from "../../helpers";
 
-export interface CoreApiProps extends Omit<RestApiProps, "restApiName"> {}
+export interface CoreApiProps extends Omit<RestApiProps, "restApiName"> {
+  rootDomain?: string;
+  subDomain?: string;
+  certificateArn?: string;
+}
 
-export class CoreApi extends RestApi {
-  constructor(scope: Construct, id: string, props?: CoreApiProps) {
+export class OrionApi extends RestApi {
+  constructor(scope: Construct, id: string, props: CoreApiProps) {
     super(scope, id, {
       ...props,
       restApiName: buildResourceName(id),
@@ -15,15 +26,39 @@ export class CoreApi extends RestApi {
       },
       defaultCorsPreflightOptions: {
         allowHeaders: [
-          'Content-Type',
-          'X-Amz-Date',
-          'Authorization',
-          'X-Api-Key',
+          "Content-Type",
+          "X-Amz-Date",
+          "Authorization",
+          "X-Api-Key",
         ],
-        allowMethods: ['OPTIONS', 'GET', 'POST'],
+        allowMethods: ["OPTIONS", "GET", "POST"],
         allowCredentials: true,
-        allowOrigins: ['http://localhost:3000'],
+        allowOrigins: ["http://localhost:3000", "https://*.oriontools.ai"],
       },
     });
+
+    const { rootDomain, subDomain, certificateArn } = props;
+
+    if (certificateArn && rootDomain && subDomain) {
+      const certificate = Certificate.fromCertificateArn(
+        this,
+        "Certificate",
+        certificateArn
+      );
+
+      this.addDomainName("DomainName", {
+        domainName: `${subDomain}.${rootDomain}`,
+        certificate: certificate,
+        endpointType: EndpointType.EDGE,
+      });
+
+      new ARecord(this, "ApiSubDomainDNS", {
+        zone: HostedZone.fromLookup(this, "baseZone", {
+          domainName: rootDomain,
+        }),
+        recordName: subDomain,
+        target: RecordTarget.fromAlias(new ApiGateway(this)),
+      });
+    }
   }
 }
---
+++ services/core/resources/stacks/demo-stack.ts
@@ -0,0 +1,43 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { Table } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+import { DemoReviewLambda } from "../../functions/demo-review-lambda/config";
+import { getCertificateArn, getDomainName } from "../../helpers";
+import { OrionApi } from "../constructs/api-gateway";
+import { ReviewBucket } from "../constructs/review-bucket";
+
+interface DemoStackProps extends StackProps {
+  stage: string;
+  userTable: Table;
+}
+
+export class DemoStack extends Stack {
+  constructor(scope: Construct, id: string, props: DemoStackProps) {
+    super(scope, id, props);
+
+    // Separate API for the demo to enable strong throttling
+    const demoApi = new OrionApi(this, "demo-api", {
+      deployOptions: {
+        throttlingBurstLimit: 1,
+        throttlingRateLimit: 1,
+      },
+      rootDomain: getDomainName(props.stage),
+      subDomain: "demo",
+      certificateArn: getCertificateArn(this, "demo"),
+    });
+
+    //Resources
+    const demoReviewBucket = new ReviewBucket(this, "demo-review-bucket");
+
+    //Lambda
+    const demoReviewLambda = new DemoReviewLambda(this, "demo-review-lambda", {
+      table: props.userTable,
+      bucket: demoReviewBucket,
+    });
+
+    //Routes
+    const demoReviewRoute = demoApi.root.addResource("demoReview");
+    demoReviewRoute.addMethod("POST", new LambdaIntegration(demoReviewLambda));
+  }
+}
