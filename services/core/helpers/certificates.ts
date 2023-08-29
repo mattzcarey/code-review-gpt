ed300d8774e8566dc5ba7b592afce60533cf9432
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-08-29 13:38:36 +0100

    fix: isDev()

--- services/core/helpers/certificates.ts
+++ services/core/helpers/certificates.ts
@@ -1,19 +1,23 @@
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { Construct } from "constructs";
 import { getStage } from "./env-helpers";
+import { isDev } from "./environment";
 
 export const getCertificateArn = (
   scope: Construct,
   subDomain: string
 ): string | undefined => {
   const stage = getStage();
-  return (
-    StringParameter.fromStringParameterAttributes(
-      scope,
-      `CertificateArnParameter-${subDomain}-${stage}`,
-      {
-        parameterName: `${subDomain.toUpperCase()}_${stage.toUpperCase()}_CERTIFICATE_ARN`,
-      }
-    ).stringValue ?? undefined
-  );
+
+  if (isDev()) {
+    return undefined;
+  }
+
+  return StringParameter.fromStringParameterAttributes(
+    scope,
+    `CertificateArnParameter-${subDomain}-${stage}`,
+    {
+      parameterName: `${subDomain.toUpperCase()}_${stage.toUpperCase()}_CERTIFICATE_ARN`,
+    }
+  ).stringValue;
 };
