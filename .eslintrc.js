97a3290e76a4a7c8d39ad6ab9cc8dd75e6e56325
April Bates <aprilb@aleios.com>
2023-08-30 12:29:52 +0100

    feat: editing eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "standard-with-typescript",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  ignorePatterns: ["**.js"],
+  extends: "standard-with-typescript",
+  rules: {},
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.eslint.json"],
+    tsconfigRootDir: __dirname,
+  },
+};
