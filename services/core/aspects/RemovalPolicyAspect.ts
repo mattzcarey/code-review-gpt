73656c40c28daa861a9b2048cdc995b462fae41b
April Bates <aprilb@aleios.com>
2023-08-30 15:25:50 +0100

    fix: removing empty constructor

--- services/core/aspects/RemovalPolicyAspect.ts
+++ services/core/aspects/RemovalPolicyAspect.ts
@@ -4,7 +4,6 @@ import { IConstruct } from "constructs";
 import { isProduction } from "../helpers";
 
 export class RemovalPolicyAspect implements IAspect {
-  constructor() {}
   visit(node: IConstruct): void {
     if (node instanceof CfnResource && !isProduction()) {
       node.applyRemovalPolicy(RemovalPolicy.DESTROY);
