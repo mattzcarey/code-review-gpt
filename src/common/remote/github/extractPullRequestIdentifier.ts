7513c520375322cb0f6b30a383debcfd036720cd
April Bates <aprilb@aleios.com>
2023-08-31 15:48:05 +0100

    fix: cr comment- removing unused code

--- src/common/remote/github/extractPullRequestIdentifier.ts
+++ src/common/remote/github/extractPullRequestIdentifier.ts
@@ -1,8 +1,7 @@
 import { PullRequestIdentifier } from './types';
 
 export const extractPullRequestIdentifier = (identifier: string): PullRequestIdentifier => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [owner, _, repo, __, prId] = identifier.split(/(\/|#)/);
+  const [owner, repo, prId] = identifier.split(/(\/|#)/);
   const prNumber = parseInt(prId);
 
   return { owner, repo, prNumber };
