14d01621c172885c8e41111522cb6daa10f2bf2e
April Bates <aprilb@aleios.com>
2023-09-01 15:11:28 +0100

    fix: readding removed code

--- services/web-app/types/next-auth.d.ts
+++ services/web-app/types/next-auth.d.ts
@@ -14,3 +14,16 @@ declare module "next-auth" {
     id?: string;
   }
 }
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    refreshTokenExpires?: number;
+    accessTokenExpires?: number;
+    refreshToken?: string;
+    token: string;
+    exp?: number;
+    iat?: number;
+    jti?: string;
+  }
+}
