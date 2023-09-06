e4bf82b4652ac104453622fd620306be83fff1de
Luca Guidi <me@lucaguidi.com>
2023-09-06 11:58:52 +0200

    Removed redundant test

--- src/common/remote/github/extractPullRequestIdentifier.test.ts
+++ src/common/remote/github/extractPullRequestIdentifier.test.ts
@@ -3,10 +3,7 @@ import { PullRequestIdentifier } from './types';
 
 describe("extract Pull Request Identifier", () => {
   it("Extracts from valid identifiers", () => {
-    const repo1 = new PullRequestIdentifier("mattzcarey", "code-review-gpt", 96);
-    expect(extractPullRequestIdentifier("mattzcarey/code-review-gpt#96")).toEqual(repo1);
-
-    const repo2 = new PullRequestIdentifier("toptal", "blackfish", 9148);
-    expect(extractPullRequestIdentifier("toptal/blackfish#9148")).toEqual(repo2);
+    const repo = new PullRequestIdentifier("mattzcarey", "code-review-gpt", 96);
+    expect(extractPullRequestIdentifier("mattzcarey/code-review-gpt#96")).toEqual(repo);
   });
 });
