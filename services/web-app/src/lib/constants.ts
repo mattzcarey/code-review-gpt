1d40b4b75c2650f824d0c482a24ced7603d73dff
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-09-07 16:05:57 +0100

    feat: push url

--- services/web-app/src/lib/constants.ts
+++ services/web-app/src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   "https://9e0fli8qi6.execute-api.eu-west-2.amazonaws.com/prod";
 export const DEMO_URL =
-  " https://n6r2v56qbg.execute-api.eu-west-2.amazonaws.com/prod";
+  "https://n6r2v56qbg.execute-api.eu-west-2.amazonaws.com/prod";
