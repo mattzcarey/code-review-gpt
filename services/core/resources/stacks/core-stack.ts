889168edd604bcc9809aa3c3cef38bc6449e46c7
Sebastian Oliver <sebo@aleios.com>
2023-09-15 13:07:35 +0200

    feat: addRepo lambda

--- services/core/resources/stacks/core-stack.ts
+++ services/core/resources/stacks/core-stack.ts
@@ -9,6 +9,8 @@ import { UpdateUserLambda } from "../../functions/update-user/config";
 import { getCertificateArn, getDomainName, getStage } from "../../helpers";
 import { OrionApi } from "../constructs/api-gateway";
 import { UserTable } from "../constructs/user-table";
+import { AddRepoLambda } from '../../functions/add-repo/config';
+import { Lambda } from 'aws-cdk-lib/aws-ses-actions';
 
 interface CoreStackProps extends StackProps {
   stage: string;
@@ -44,11 +46,18 @@ export class CoreStack extends Stack {
       table: this.userTable,
     });
 
+    const addRepoLambda = new AddRepoLambda(this, "add-repo-lambda", {
+      table: this.userTable,
+    });
+
     //Routes
     const updateUserRoute = api.root.addResource("updateUser");
     updateUserRoute.addMethod("POST", new LambdaIntegration(updateUserLambda));
 
     const getUserRoute = api.root.addResource("getUser");
     getUserRoute.addMethod("GET", new LambdaIntegration(getUserLambda));
+
+    const addRepoRoute = api.root.addResource("addRepo");
+    addRepoRoute.addMethod("POST", new LambdaIntegration(addRepoLambda));
   }
 }
