c598cf2c4d036ca78b027acb377279ae739d37bd
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-11-14 19:33:36 +0200

    chore: fix out of bounds

--- services/core/functions/webhook/src/review/comment/findLineInPatch.ts
+++ services/core/functions/webhook/src/review/comment/findLineInPatch.ts
@@ -15,14 +15,14 @@ export const findPositionsFromSnippet = (
   }
 
   // Found the first line of the snippet
-  for (let i = 0; i <= lines.length; i++) {
+  for (let i = 0; i < lines.length; i++) {
     if (lines[i].includes(snippetLines[0].trim())) {
       // If the snippet is only one line, return the first line
       if (snippetLines.length === 1) {
         return { firstLine: undefined, lastLine: i };
       }
       // If the snippet is more than one line, check if the next line is the second line of the snippet
-      if (lines[i + 1].includes(snippetLines[1].trim())) {
+      if (i + 1 > lines.length && lines[i + 1].includes(snippetLines[1].trim())) {
         return { firstLine: i, lastLine: i + snippetLines.length - 1 };
       }
       // If the next line is not the second line of the snippet, go to the next line
@@ -30,6 +30,7 @@ export const findPositionsFromSnippet = (
     }
   }
 
+  // Not found, return the last line of the file
   return {
     firstLine: undefined,
     lastLine: lines.length - 1,
