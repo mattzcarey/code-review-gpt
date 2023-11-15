24325e69175344ba20584b9a5e9cf668246707f6
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-11-15 18:18:11 +0200

    try library

--- services/core/functions/webhook/src/chat/chat.ts
+++ services/core/functions/webhook/src/chat/chat.ts
@@ -1,6 +1,9 @@
+/* eslint-disable max-depth */
 /* eslint-disable complexity */
 // Reviewing multiple files inline > prioritising them > adding review comments
 // Answer questions > get the comments on the PR (by me and the questioner) as context > answer the question as comment
+
+import jsesc from "jsesc";
 
 import { modelInfo } from "../constants";
 import { AIModel } from "../llm/ai";
@@ -38,7 +41,6 @@ export class Chat {
     const prompt = buildReviewPrompt(patch);
     const maxPromptLength = this.getMaxPromptLength(this.modelName);
 
-    // TODO: fix this hack
     if (prompt.length > maxPromptLength) {
       console.error(
         `File ${prompt} is too large to review, skipping review for this file`
@@ -47,125 +49,49 @@ export class Chat {
       return undefined;
     }
 
-    //call model
-    const jsonResponse = await this.ai.callModel(prompt);
-
-    //trim the response to remove the ```json tags
-    const cleanJsonResponse = removeMarkdownJsonQuotes(jsonResponse);
-
-    // several strategies to try to parse the json
     try {
-      //try to parse the json normally
-      let reviewData: ReviewFile[];
+      let jsonResponse = await this.ai.callModel(prompt);
+      jsonResponse = removeMarkdownJsonQuotes(jsonResponse);
+
       try {
-        reviewData = JSON.parse(cleanJsonResponse) as ReviewFile[];
-      } catch (error) {
+        return JSON.parse(jsonResponse) as ReviewFile[];
+      } catch (parseError) {
         console.error(
-          `Error parsing JSON from AI: ${
-            (error as Error).message
-          }. Trying to recover. Data: ${cleanJsonResponse}`
+          `Error parsing JSON: ${
+            (parseError as Error).message
+          }. Escaping special characters and retrying.`
         );
+
+        try {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+          const escapedJsonResponse: string = jsesc(jsonResponse, {
+            json: true,
+          });
+
+          return JSON.parse(escapedJsonResponse) as ReviewFile[];
+        } catch (escapeParseError) {
+          console.error(
+            `Error parsing escaped JSON: ${
+              (escapeParseError as Error).message
+            }. Returning undefined.`
+          );
+
+          return undefined;
+        }
       }
-
-      //if we failed to parse the json, try to fix it
-      let encodedJsonResponse: string;
-      let decodedReviewData: ReviewFile[];
-
-      //encoding the Suggested Code and Code Snippet fields in base64
-      try {
-        encodedJsonResponse = encodeCodeInJson(jsonResponse);
-      } catch (error) {
-        throw new Error(
-          `Error encoding JSON: ${
-            (error as Error).message
-          }. Data: ${jsonResponse}`
-        );
-      }
-
-      //try to parse the json again
-      try {
-        reviewData = JSON.parse(encodedJsonResponse) as ReviewFile[];
-      } catch (error) {
-        throw new Error(
-          `Error parsing encoded JSON from AI: ${
-            (error as Error).message
-          }. Data: ${encodedJsonResponse}`
-        );
-      }
-
-      // decoding the Suggested Code and Code Snippet fields from base64
-      try {
-        decodedReviewData = decodeCodeInReviewData(reviewData);
-      } catch (error) {
-        throw new Error(
-          `Error decoding JSON: ${
-            (error as Error).message
-          }. Data: ${JSON.stringify(reviewData)}`
-        );
-      }
-
-      return decodedReviewData;
     } catch (error) {
       console.error(
-        `Error parsing JSON using encode/decode from AI: ${
-          (error as Error).message
-        }. Stack: ${(error as Error).stack as string} Data: ${jsonResponse}`
+        `Error processing review data: ${(error as Error).message}`
       );
-      // Last chance... Failed to encode and decode the suggestions. Set them to "" and then parse the json.
-      try {
-        return JSON.parse(
-          removeSuggestionsFromJSON(cleanJsonResponse)
-        ) as ReviewFile[];
-      } catch (fixError) {
-        console.error(
-          `Error fixing JSON from AI. Stack:${
-            (fixError as Error).stack as string
-          }. Returning undefined. Data: ${cleanJsonResponse}`
-        );
 
-        return undefined;
-      }
+      return undefined;
     }
   };
 }
 
 const removeMarkdownJsonQuotes = (jsonString: string): string => {
-  // Remove Markdown code block syntax from the start of the JSON string
   return jsonString
     .replace(/^`+\s*json\s*/, "")
     .replace(/\s*`+$/, "")
     .trim();
-};
-
-const removeSuggestionsFromJSON = (jsonString: string): string => {
-  return jsonString.replace(/("suggestedCode":\s*").+?(",)/g, '$1""$2');
-};
-
-const encodeCodeInJson = (jsonString: string): string => {
-  return jsonString.replace(
-    /("suggestedCode":\s*")(.+?)(",|"codeSnippet":)/g,
-    (p1: string, p2: string, p3: string) => {
-      const encodedCode = Buffer.from(p2).toString("base64");
-
-      return p1 + encodedCode + p3;
-    }
-  );
-};
-
-const decodeCodeInReviewData = (reviewData: ReviewFile[]): ReviewFile[] => {
-  reviewData.forEach((review) => {
-    if (review.suggestedCode) {
-      review.suggestedCode = Buffer.from(
-        review.suggestedCode,
-        "base64"
-      ).toString("utf-8");
-    }
-    if (review.codeSnippet) {
-      review.codeSnippet = Buffer.from(review.codeSnippet, "base64").toString(
-        "utf-8"
-      );
-    }
-  });
-
-  return reviewData;
 };
