3e6fe745a8e07f335d0ccd466ae539500de2a929
Matt Carey <matt@stackone.com>
2024-07-15 18:31:53 +0100

    fix: tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
     include: ["src/**/*.test.ts"],
     environment: "node",
-  },
-});
+    globals: true
+  }
+})
