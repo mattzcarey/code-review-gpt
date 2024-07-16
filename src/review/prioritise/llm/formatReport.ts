205fe87eba03560d4fd055f4ceca935964cf7d83
Matt Carey <matt@stackone.com>
2024-07-16 17:16:07 +0100

    format test

--- src/review/prioritise/llm/formatReport.ts
+++ src/review/prioritise/llm/formatReport.ts
@@ -2,18 +2,24 @@ import { type IFeedback } from "../../../common/model/AIModel"
 
 const formatFeedback = (
   feedback: IFeedback
-): string => `**${feedback.riskScore}/5 - ${feedback.summary}**
+): string => `## ${feedback.riskScore}/5 - ${feedback.summary}
 <details>
 <summary>More details</summary>
 
-Filename: ${feedback.fileName}
-Score: ${feedback.riskScore}/5 (lower is better)
-Review: 
+
+### Filename
+
+${feedback.fileName}
+
+### Score
+
+${feedback.riskScore}/5 (lower is better)
+
+### Review
+
 ${feedback.details}
 </details>
 `
 
 export const formatReport = (feedbacks: IFeedback[], summary: string): string => `
-${feedbacks.map(formatFeedback).join("\n\n---\n\n")}
----
-${summary}\n`
+${feedbacks.map(formatFeedback).join("\n\n---\n\n")}\n\n${summary}\n`
