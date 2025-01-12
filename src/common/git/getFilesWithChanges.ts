a1d497b27ffdf97dbe396e02c58208d75503bff8
Matt Carey <matt@stackone.com>
2025-01-12 15:34:27 +0000

    format

--- src/common/git/getFilesWithChanges.ts
+++ src/common/git/getFilesWithChanges.ts
@@ -31,6 +31,8 @@ export const getFilesWithChanges = async (isCi: string | undefined): Promise<Rev
 
     return files;
   } catch (error) {
-    throw new Error(`Failed to get files with changes: ${(error as Error).message}\n\n${(error as Error).stack}`);
+    throw new Error(
+      `Failed to get files with changes: ${(error as Error).message}\n\n${(error as Error).stack}`
+    );
   }
 };
