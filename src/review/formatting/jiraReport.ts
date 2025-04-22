056a1e7aaa1de55f52475e495c6adcec4b72b59c
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2025-04-22 18:13:51 +0300

    Update jiraReport.ts

--- src/review/formatting/jiraReport.ts
+++ src/review/formatting/jiraReport.ts
@@ -4,14 +4,15 @@ import type { IFeedback, IReviews } from '../../common/types';
 export const formatReviewForJira = (reviews: IReviews): string => {
   return reviews
     .map((review) => `${review.reasoning}`)
-    .join(' \\n');
+    .join('\\n\\n');
 };
 
 // Format feedback for Jira
-const formatFeedbackForJira = (feedback: IFeedback): string => `*Risk Level ${feedback.riskScore} - ${feedback.fileName}*
-\\n\\n
+const formatFeedbackForJira = (feedback: IFeedback): string => `
+{panel:title=Risk Level ${feedback.riskScore} - ${feedback.fileName}|borderStyle=dashed|borderColor=#ccc|titleBGColor=#f7f7f7|bgColor=#fff}
 ${formatReviewForJira(feedback.review)}
-\\n\\n`;
+{panel}
+`;
 
 // Generate Jira report
 export const jiraReport = (feedbacks: IFeedback[]): string => `${feedbacks.map(formatFeedbackForJira).join('\n----\n')}`; 
